b30e8c7072ea12233a7b3743ba6e669ed878024c
Moritz <moritz@borries.email>
2024-01-07 19:54:43 +0100

    w

--- src/Gun.java
+++ src/Gun.java
@@ -2,8 +2,8 @@ import greenfoot.GreenfootImage;
 
 public class Gun extends Items {
     public Gun(int width, int height) {
-        GreenfootImage cristal = new GreenfootImage("Gun.png");
-        cristal.scale(width, height);
-        setImage(cristal);
+        GreenfootImage gun = new GreenfootImage("Gun.png");
+        gun.scale(width, height);
+        setImage(gun);
     }
 }
