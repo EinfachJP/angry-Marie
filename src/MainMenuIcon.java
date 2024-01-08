ba017551ccf9f8247da9b36387662b51c31bd42c
Moritz <moritz@borries.email>
2024-01-08 17:02:09 +0100

    Menu

---
+++ src/MainMenuIcon.java
@@ -0,0 +1,30 @@
+import greenfoot.GreenfootImage;
+
+public class MainMenuIcon extends BlockingObject{
+
+
+
+
+    public MainMenuIcon(){
+        GreenfootImage mainMenuIcon = new GreenfootImage("StoppButton.png");
+        mainMenuIcon.scale(40,40);
+        setImage(mainMenuIcon);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // Importe für Greenfoot-Klassen
 
-public class Menü extends World {
+public class Menu extends World {
     private static final int BUTTON_WIDTH = 150;
     private static final int BUTTON_HEIGHT = 50;
 
-    public Menü() {
+    public Menu() {
         super(1, 1, 40);  // Größe der Welt
         GreenfootImage cell2 = new GreenfootImage("cell.Teleporter.jpg");
         cell2.scale(180, 180);
@@ -15,7 +15,7 @@ public class Menü extends World {
 
     private void prepareMenu() {
         // Füge Buttons zum Menü hinzu
-        addObject(new StartButton(), getWidth() / 2, getHeight() / 2 - 50);
-        addObject(new QuitButton(), getWidth() / 2, getHeight() / 2 + 50);
+        addObject(new StartButton(), 1, 21);
+        addObject(new QuitButton(), 2, 21);
     }
 }
